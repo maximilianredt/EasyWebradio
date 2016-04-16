daea066031fbac4a3276b44a73d3b1e8320f0587
Maximilian Redt <mr@wesolix.com>
2016-04-17 00:17:13 +0200

    refactored app.js & app/Player.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,39 +4,57 @@ $.getScript('assets/js/app/Player.js', function()
 {
 	var player = new Player('http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p');
 	//var player = new Player('http://swr-mp3-m-swr3.akacast.akamaistream.net/7/720/137136/v1/gnl.akacast.akamaistream.net/swr-mp3-m-swr3');
-	var playerCtrl = $('.player-control-play');
-	playerCtrl.attr('data-status', player.status);
-	var slider = $('.player-control-volume .slider'),
+	var playerCtrl = $('.player-control-play'),
+		slider = $('.player-control-volume .slider'),
 		tooltip = $('.player-control-volume .tooltip');
 
+	// initially write the stream status
+	playerCtrl.attr('data-status', player.status);
+
+	// hide tooltip for now until we need it ;)
 	tooltip.hide();
 
+	/**
+	 * init volume slider
+	 */
 	slider.slider({
 		'value': 100,
 
 		start: function(event,ui) {
+			// show tooltip when sliding starts
 			tooltip.fadeIn('fast');
 		},
 
 		slide: function(event, ui) {
-
+			// get new volume
 			var value = slider.slider('value');
 
+			// set new volume in tooltip
 			tooltip.css('left', value).text(ui.value);
 
+			// set new volume in player
 			player.setVolume(ui.value);
 		},
 
 		stop: function(event,ui) {
+			// hide tooltip when sliding starts
 			tooltip.fadeOut('fast');
 		}
 	});
 
 
+	/**
+	 * value of Player.status should always be on
+	 * '.player-control-volume .slider[data-status]'
+	 * to handle icons on button via css.
+	 */
 	player.watch('status', function(prop, oldval, newval) {
 		playerCtrl.attr('data-status', newval);
 	});
 
+	/**
+	 * toggle the stream on click
+	 */
 	playerCtrl.on('click', function() {
 		switch (playerCtrl.attr('data-status')) {
 			case 'stopped':
--- assets/js/app/Player.js
+++ assets/js/app/Player.js
@@ -5,9 +5,11 @@ var Player = function(stream_url) {
 	this.event = {};
 	var self = this;
 
+	// create Audio object and set the stream initial status to "loading"
 	this.stream = new Audio(this.stream_url);
 	this.status = 'loading';
 
+	// link events with handling functions
 	this.stream.oncanplaythrough = function() {
 		self.oncanplaythrough(this);
 	};
@@ -16,23 +18,43 @@ var Player = function(stream_url) {
 	};
 };
 
+/**
+ * prepare playing of stream,
+ * but for now only load the stream.
+ * oncanplaythrough will start the stream then,
+ * as soon as it is loaded.
+ */
 Player.prototype.play = function() {
 	this.stream.load();
 	this.status = 'loading';
 };
+
+/**
+ * stop stream
+ */
 Player.prototype.stop = function() {
 	this.stream.pause();
 	this.status = 'stopped';
 };
+
+/**
+ * change the volume of the stream
+ */
 Player.prototype.setVolume = function(volume) {
 	this.stream.volume = volume / 100;
 };
 
-
+/**
+ * start stream when it is ready and update stream status
+ */
 Player.prototype.oncanplaythrough = function(stream) {
 	stream.play();
 	this.status = 'playing';
 };
+
+/**
+ * update stream status on pause
+ */
 Player.prototype.onpause = function() {
 	this.status = 'stopped';
 };
